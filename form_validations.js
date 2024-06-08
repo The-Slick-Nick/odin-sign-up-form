3b311e84aa63917c5d638782ac07e301f69fa39f
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-06-07 23:04:56 -0500

    Add more complex validation behaviour

--- form_validations.js
+++ form_validations.js
@@ -1,12 +1,28 @@
 /* pwd_validate.js
  * 
- * This script provides a means for checking the password and password validation fields, assigning
- * them an invalid class if they do not match.
+ * This script 
  */
 
 
 const pwdInput = document.querySelector("#password");
 const pwdConfirm = document.querySelector("#password-confirm");
+const submitButton = document.querySelector(".submit-block>button");
+const allInputs = document.querySelectorAll("input");
+
+
+/* Don't identify fields as invalid unless they've been selected at least once */
+allInputs.forEach((inputElem) => {
+    inputElem.addEventListener("focus", (event) => {
+        event.target.classList.add("validation-enabled");
+    });
+});
+
+submitButton.addEventListener("click", (event) => {
+    allInputs.forEach((inputElem) => {
+        inputElem.classList.add("validation-enabled");
+    });
+});
+
 
 let verifyValuesEqual = function(input1, input2) {
     if (input1.value !== input2.value) {
